968fac67ca91086aebc4ef8103721f0f6a33af75
wioleta <wioleta0294@outlook.com>
2015-10-19 06:07:18 +0200

    plansze

---
+++ KckSokoban/Interfejsy/IPlansza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KckSokoban.Interfejsy
+{
+    interface IPlansza
+    {
+      
+        void wczytajPlansze(int level);
+    
+         void rysujPlansze();
+   
+         void odswierzCele();
+        
+
+         void koniecGry();
+    
+         void ruszaj(char gdzie);
+        
+         void odswierz(int pozX, int pozY);
+        
+
+    }
+}
